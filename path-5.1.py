4244da8accef1384956cce417c90b10c4c79ca3f
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-16 12:49:15 +0200

    Added function to main task

--- path-5.1.py
+++ path-5.1.py
@@ -17,7 +17,7 @@ def open_path(path_name):
     return os.listdir(path_name)
 
 
-def find_names_startswith_deep(dirs_name):
+def extract_names_startswith_deep(dirs_name):
     """
     :param dirs_name: list with dirs name
     :return: files are start with "deep"
@@ -25,9 +25,17 @@ def find_names_startswith_deep(dirs_name):
     return [file for file in dirs_name if file.startswith("deep")]
 
 
+def print_names_startswith_deep(path):
+    """
+    Prints the name are start with deeps
+    :param path:
+    :return:
+    """
+    dirs = open_path(path)
+    deeps = extract_names_startswith_deep(dirs)
+    print(deeps)
+
+
 if __name__ == '__main__':
     path = """C:\\Users\\tzomi\\source"""
-    dirs = open_path(path)
-
-    deeps = find_names_startswith_deep(dirs)
-    print(deeps)
+    print_names_startswith_deep(path)
