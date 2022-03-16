ceae532ec502d323b42fb059ad2f6a050896e9db
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-16 13:11:42 +0200

    Added images directory

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
 import path
 
 if __name__ == '__main__':
-   # path.print_names_startswith_deep("""C:\\Users\\tzomi\\source""")
-
-
+    path.print_names_startswith_deep("""D:\\yam-mesika-weeks-5-6-YinonTzo\\images""")
+    #print("hello")
