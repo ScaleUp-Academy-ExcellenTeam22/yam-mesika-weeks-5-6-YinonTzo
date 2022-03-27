79c7e07cbdb00cadccbaf21179b71fa260b5fadf
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-27 13:38:41 +0300

    Added hints, and more tests in main, changed optinal's default argument and more things from Tal

--- cup_of_cake.py
+++ cup_of_cake.py
@@ -1,22 +1,21 @@
-import functools
-
-
-def get_recipe_price(prices, optionals=[], **ingredients):
+def get_recipe_price(prices: dict, optionals: list = None, **ingredients: int) -> float:
     """
-
     :param prices: dictionary of prices and products
-    :param optionals: optional ingredients to canceling
+    :param optionals: optional list ingredients to canceling
     :param ingredients: to pick up
     :return: total sum of products
     """
+    if optionals is None:
+        optionals = []
+
     total = 0
-    PRICE = 1
-    PRODUCT = 0
-    for product in ingredients.items():
-        if product[0] not in optionals:
-            total += (float(product[PRICE]) / 100) * prices[product[PRODUCT]]
+    for product, amount in ingredients.items():
+        if product not in optionals:
+            total += ((float(amount) / 100) * prices[product])
     return total
 
 
 if __name__ == '__main__':
     print(get_recipe_price({}))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300))
--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    nums = [str(n) for n in range(20)]
+    a = " ".join(nums)
+    print(nums)
+    print(a)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
