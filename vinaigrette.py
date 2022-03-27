6205e205c67e80c0100c5183cb4a84a7d7be0b67
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-27 15:33:15 +0300

    Refactor documentation

--- vinaigrette.py
+++ vinaigrette.py
@@ -2,23 +2,30 @@ import random
 import time
 import datetime
 
+"""
+Reads two dates in form YYYY-MM-DD, draw new date between them.
+If the new date is Monday, the program will print "I don't have vinaigrette".
+dates example: 2022-3-14 is Monday
+2022-3-13
+2022-3-15
+"""
 
-def random_date(start, end, time_format, random_number):
-    """
-    The function parses the strings start and end to struct_time
-    then, converts them to float, multiplies by random
-    and converts it to local time.
-    :param start:
-    :param end:
-    :param time_format: time format of start and end. for example
-    %Y-%m-%d, %m/%d/%Y %I:%M %p etc
-    :param random_number: random number to adding to start
-    :return: date representing by '2022-3-16'
-    """
-    stime = time.mktime(time.strptime(start, time_format))
-    etime = time.mktime(time.strptime(end, time_format))
 
-    ptime = stime + random_number * (etime - stime)
+def random_date(start_date: str, end_date: str, time_format: str, random_number: random) -> datetime:
+    """
+    The function parses the strings start_date and end_end to struct_time then,
+    converts them to float, multiplies by random and converts it to local time.
+    :param start_date: Start date in form YYYY-MM-DD.
+    :param end_date: End date in form YYYY-MM-DD.
+    :param time_format: Time format of start and end. for example
+    %Y-%m-%d, %m/%d/%Y %I:%M %p etc.
+    :param random_number: Random number to adding to start.
+    :return: Date representing by '2022-3-16'.
+    """
+    start_time = time.mktime(time.strptime(start_date, time_format))
+    end_time = time.mktime(time.strptime(end_date, time_format))
+
+    ptime = start_time + random_number * (end_time - start_time)
 
     return datetime.date(time.localtime(ptime).tm_year,
                          time.localtime(ptime).tm_mon,
@@ -28,24 +35,18 @@ def random_date(start, end, time_format, random_number):
 def vinaigrette():
     """
     Reads two dates from user, and draws new date between them.
-    if the new date is Monday, the function prints "I don't have Vinaigrette"
-    :return: None
+    If the new date is Monday, the function will print "I don't have Vinaigrette".
     """
-    MONDAY = 0
+    monday = 0
 
-    start = input("Enter a start date\n")
-    end = input("Enter a end date\n")
+    start_date = input("Enter a start date\n")
+    end_date = input("Enter a end date\n")
 
-    new_date = random_date(start, end, '%Y-%m-%d', random.random())
+    date_between_start_to_end = random_date(start_date, end_date, '%Y-%m-%d', random.random())
 
-    if new_date.weekday() == MONDAY:
+    if date_between_start_to_end.weekday() == monday:
         print("monday")
 
 
-"""
-dates example: 2022-3-14 is Monday
-2022-3-13
-2022-3-15
-"""
 if __name__ == '__main__':
     vinaigrette()
