2962f79b4994ea83bf4c1f55f11af17e38097136
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-27 14:37:01 +0300

    Added documents, and deleted irrelevent files

--- main.py
+++
@@ -1,5 +0,0 @@
-import path
-
-if __name__ == '__main__':
-    path.print_names_startswith_deep("""D:\\yam-mesika-weeks-5-6-YinonTzo\\images""")
-    #print("hello")
--- path.py
+++ path.py
@@ -2,34 +2,33 @@ import os
 
 """
 This module implements function to
-find files are start with "deep" in some file
+find files are start with "deep" in some file.
 
-for more explanations about listdir
-https://www.tutorialspoint.com/python/os_listdir.htm
+For more explanations about listdir:
+https://www.tutorialspoint.com/python/os_listdir.htm.
 """
 
 
-def open_path(path_name):
+def open_path(path_name: str) -> list:
     """
-    :param path_name:
-    :return:list with all dirs inside @path_name
+    :param path_name: Path name to open.
+    :return:List with all dirs inside @path_name.
     """
     return os.listdir(path_name)
 
 
-def extract_names_startswith_deep(dirs_name):
+def extract_names_startswith_deep(dirs_name: list) -> list:
     """
-    :param dirs_name: list with dirs name
-    :return: files are start with "deep"
+    :param dirs_name: List with dirs name.
+    :return: Files are start with "deep".
     """
     return [file for file in dirs_name if file.startswith("deep")]
 
 
-def print_names_startswith_deep(path):
+def print_names_startswith_deep(path: str) -> None:
     """
-    Prints the name are start with deeps
-    :param path:
-    :return:
+    Prints the name are start with deeps.
+    :param path: File to open to find names.
     """
     dirs = open_path(path)
     deeps = extract_names_startswith_deep(dirs)
@@ -37,5 +36,6 @@ def print_names_startswith_deep(path):
 
 
 if __name__ == '__main__':
-    path = """C:\\Users\\tzomi\\source"""
-    print_names_startswith_deep(path)
+    path_to_open = "D:\\yam-mesika-weeks-5-6-YinonTzo\\images"
+
+    print_names_startswith_deep(path_to_open)
