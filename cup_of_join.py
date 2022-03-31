f166814f262c7981654a833e538b249c16351058
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-03-31 17:28:27 +0300

    Fix join function

--- cup_of_join.py
+++ cup_of_join.py
@@ -1,21 +1,16 @@
-def join(*lists: list, sep='-'):
-    """
 
-    :param lists: list of lists
-    :param sep: seperator
-    :return: new list with combination of lists with seperator between each
-    two list
+def join(*lists: list, seperator: str = '-') -> list or None:
+    """
+    :param lists: List of lists.
+    :param seperator: Seperator to be between the lists.
+    :return: New list with combination of lists with seperator between each list.
     """
     if not lists:
         return None
 
-    ret = []
-    for li in lists:
-        ret += li
-        ret += sep
-
-    return ret[:-1:]
+    return [li for sublist in lists for li in sublist + list(seperator)][:-1]
 
 
 if __name__ == '__main__':
-    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(join([1, 2], [8], [9, 5, 6], seperator='@'))
+
